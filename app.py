a70147a77a9ca3384e46a4e70876d06daa219eee
Oleh Smirnov <o.smirnov@gvmachines.com>
2017-09-14 09:57:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from bottle import route, template
 
 @route('/')
 def index():
-    return '<h1>Hello world!</h1>'
+    return '<h1>Hello world from dev!</h1>'
 
 @route('/<name>')
 def index(name):
-    return template('<h1>Hello {{name}}!</h1>', name=name)
+    return template('<h1>Hello from dev {{name}}!</h1>', name=name)
 
 app = application = bottle.default_app()
